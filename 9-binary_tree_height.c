8121a9d48ae4948927e0c4a309c9a793ab2d8ffc
Shirley <shircar16@gmail.com>
2021-12-01 00:47:06 -0500

    height tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,5 +8,21 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	
+	size_t height_left = 0;
+	size_t height_right = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
+
+	if (tree->left || tree->right)
+	{
+		if (height_left > height_right)
+			return (height_left + 1);
+		else
+			return (height_right + 1);
+	}
+	return (0);
 }
