305a22292df468e6f45d83a0b0f2c5b6def80c40
Shirley <shircar16@gmail.com>
2021-12-01 12:06:13 -0500

    count nodes

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,5 +8,14 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t count;
 	
+	count = 0;
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	else 
+		count = (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	return (count);
 }
