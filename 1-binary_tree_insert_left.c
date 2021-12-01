29ad7ceba03d7cf6bffbcf44d3ba4815c57fca01
ChristianMartinezTech <crismartine.e@gmail.com>
2021-12-01 13:48:18 -0500

    Fixed 1 and 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,6 +24,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = new_node;
 		new_node->parent = parent;
 		new_node->n = value;
+		new_node->left = NULL;
+		new_node->right = NULL;
 	}
 	else
 	{
