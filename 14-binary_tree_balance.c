dbf3a6ab280b2bcf87f7e16e7454850b28010876
Shirley <shircar16@gmail.com>
2021-12-01 17:20:52 -0500

    tasks

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,5 +8,21 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	
+	int height_left = 0;
+	int height_right = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	height_left = binary_tree_balance(tree->left);
+	height_right = binary_tree_balance(tree->right);
+
+	if (tree->left || tree->right)
+	{
+		if (height_left > height_right)
+			(height_left += 1);
+		else
+			(height_right += 1);
+	}
+	return (height_left - height_right);
 }
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,5 +8,21 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int depht = 0;
 	
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		binary_tree_depht(tree);
+
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+
+	if (tree->left && tree->right)
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	}
+	else
+		return (0);
 }
