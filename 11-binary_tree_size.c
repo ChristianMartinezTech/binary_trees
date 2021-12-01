ffc739b5611141c59ac22ee0d4e52f7be3cb172a
Shirley <shircar16@gmail.com>
2021-12-01 11:20:28 -0500

    calculate size of the tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,5 +8,12 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	
+	size_t size;
+
+	size=0;
+	if (tree == NULL)
+		return (0);
+	else
+		size = (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	return (size);
 }
