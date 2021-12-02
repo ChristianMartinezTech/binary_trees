1b3524b1ba4b69684d55c176237ccae4eb443b5c
Shirley <shircar16@gmail.com>
2021-12-01 19:51:48 -0500

    node's uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,5 +8,19 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	
+	binary_tree_t *uncle;
+
+	if (node == NULL)
+		return (NULL);
+
+	if (node->parent->parent == NULL)
+		return (NULL);
+		
+	if (node->parent->parent->left == node->parent) 
+	{
+		uncle = node->parent->parent->right;
+	}
+	else
+		uncle = node->parent->parent->left;
+	return (uncle);
 }
